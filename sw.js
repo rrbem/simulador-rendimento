56e35b2b52400dfb75f536e1b1465c03a300bb9f
rrbem <richardbem@gmail.com>
2026-05-30 17:03:47 -0300

    ajustes para versão V.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = 'simulador-v1';
-const assets = ['./simulador.html', './manifest.json'];
+const assets = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
