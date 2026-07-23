2274ab18d4c3d3c7ccbdb9d255d0824097505788
rrbem <richardbem@gmail.com>
2026-07-23 19:02:19 -0300

    ajustes no cabeçalho (data da att)

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-// IMPORTANTE: Mude o número da versão sempre que subir um novo ajuste
-const cacheName = 'simulador-v43';
+// IMPORTANTE: Mude o número da versão e a data sempre que subir um novo ajuste
+const VERSAO = '43';
+const DATA_ATUALIZACAO = '23/07/2026';
+const cacheName = `simulador-V.${VERSAO}`;
 const assets = [
   './',
   './index.html',
